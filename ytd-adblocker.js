4868246cdadefaff4bba6da9636c82a8b417ece7
Nesaijn <25134837+Nesaijn@users.noreply.github.com>
2020-11-18 17:18:51 +0100

    remove options

--- ytd-adblocker.js
+++ ytd-adblocker.js
@@ -5,21 +5,6 @@
 // @run-at      document-start
 // ==/UserScript==
 
-/* Change the word after the name of a setting (and after the column character) to
- * true to enable it and to false to disable it (DO NOT REMOVE THE COMMA AFTER THE WORD)
- * For example, enabled hideFeed will look like this: "hideFeed" : true,
- */
-
-var generalSettings = {
-  "hideMerch"    : false,
-  "hideHomeBanner" : true,
-  "hidePlayerAds" : true
-};
-
-/************************************/
-/* END OF USER CONFIGURABLE OPTIONS */
-/************************************/
-
 /* CSS that is added to page for hiding the elements */
 
 /* "id=comments" -> "#comments"
@@ -47,8 +32,6 @@ var style = document.createElement('style');
 
 // Execute general settings, only if the timeout isn't active
 for (setting in generalSettings) {
-  if (!generalSettings[setting]) continue;
-
   style.textContent += css[setting];
 }
 
